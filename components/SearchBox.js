d1ab5ba15f19ec73889fc8eb670304548db8b025
ngocLHAnlab <86952245+ngocLHAnlab@users.noreply.github.com>
2022-08-26 14:42:29 +0700

    code weather

---
+++ components/SearchBox.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import Link from 'next/link';
+import cities from "../lib/city.list.json";
+import { Router } from 'next/router';
+// import cities from "cities.json";
+export default function SearchBox({placeholder}) {
+    const [query, setQuery] = useState("");
+    const [results, setResults] = useState([]);
+    const [index, setIndex] = useState(0)
+
+    // React.useEffect(() => {
+    //   const clearQuery = () => setQuery("");
+    //   Router.events.on("routeChangeComplete", clearQuery);
+
+    //   return () =>{
+    //     Router.events.off("routeChangeComplete", clearQuery);
+    //   }
+    // }, []);
+
+    const setSuggestHandler = (name, country) =>{
+        setQuery(name)
+        setResults([])
+        document.location = "/location/"+name
+        setIndex(0)
+      }
+    const onChangeHandler = (text)=>{
+        var matches = []
+        var topten  = []
+        if (text.length>0) {
+          matches = cities.filter((city) => {
+            return city.name.toLowerCase().startsWith(text.toLowerCase());
+          });
+          topten = matches.slice(0, 10);
+        }
+        if(index >= topten.length){
+          setIndex(0)
+        }
+        setResults(topten)
+        setQuery(text)
+      }
+      const handleCheckKey = (e) =>{
+        if(results.length>0){
+          if(e.keyCode === 38){
+            if(index > 0){
+              setIndex(index-1)
+            }
+          }else if (e.keyCode === 40){
+            if(index < results.length-1){
+              setIndex(index+1)
+            }
+          }else if(e.keyCode === 13){
+            setSuggestHandler(results[index].name, results[index].country)
+          }
+        }
+      }
+  return (
+    <div className='search'>
+        <input type={"text"} value={query}
+            placeholder={placeholder ? placeholder : ""}
+            onKeyDown={handleCheckKey}
+            onChange={e => onChangeHandler(e.target.value)} />
+        {results.length > 0 && (
+            <ul>
+                {results.map((city, i) => (
+                  <li key={city.slug}
+                    style={index === i ? {backgroundColor:'#DDD'} : {backgroundColor:'#FFF'}}
+                    onClick={()=> setSuggestHandler(city.name, city.country)}>
+                      {city.name}
+                      <span> ({city.country})</span>
+                  </li>
+                ))}
+            </ul>
+        )}
+    </div>
+  )
+}
---
+++ pages/location/[city].js
@@ -0,0 +1,81 @@
+import Head from 'next/head';
+import React from 'react'
+import cities from "../../lib/city.list.json";
+import weather from "../../lib/weather.json";
+import weathers from "../../lib/weathers.json";
+import TodaysWeather from '../../components/TodaysWeather';
+import moment from 'moment-timezone';
+import HourlyWeather from '../../components/HourlyWeather';
+import WeeklyWeather from '../../components/WeeklyWeather';
+import SearchBox from '../../components/SearchBox';
+// import cities from "cities.json";
+export async function getServerSideProps(context) {
+    const city = getCity(context.params.city);
+    if(!city){
+        return {
+            notFound: true,
+        };
+    };
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.name},${city.country}&APPID=${process.env.API_KEY}`;
+    const url = `https://openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.API_KEY}`;
+    const res = await fetch(url);
+    const data = weathers//await res.json();
+
+    if(!data){
+        return {
+            notFound: true,
+        };
+    };
+    const slug = context.params.city;
+
+    return{
+        props: {
+            city: city,
+            cuttentWeather: data.current,
+            dailyWeather: data.daily,
+            hourlyWeather: getHourlyWeather(data.hourly, data.timezone),
+            timezone: data.timezone_offset
+        },
+    }
+}
+
+const getCity = name => {
+    const city = cities.find(city => city.name == name);
+    if (city){
+        return city;
+    } else{
+        return null;
+    }
+}
+
+const getHourlyWeather = (hourlyData, timezone) => {
+    const endOfDay = moment().tz(timezone).endOf('day').valueOf();
+    const endTimeStamp = Math.floor(endOfDay/1000);
+    const todayData = hourlyData.filter(data => data.dt < endTimeStamp);
+
+    return todayData;
+}
+export default function City({
+    hourlyWeather,
+    cuttentWeather,
+    dailyWeather,
+    city,
+    timezone
+}) {
+    console.log(city);
+    return (
+        <div>
+        <Head>
+            <title>{city.name} Weather - Next Weather App</title>
+        </Head>
+            <div className='page-wrapper'>
+                <div className='container'>
+                    <SearchBox placeholder="Search for another location..."/>
+                    <TodaysWeather city={city} weather={dailyWeather[0]} timezone={timezone}/>
+                    <HourlyWeather hourlyWeather={hourlyWeather} timezone={timezone}/>
+                    <WeeklyWeather weeklyWeather={dailyWeather} timezone={timezone} />
+                </div>
+            </div>
+        </div>
+    );
+}
